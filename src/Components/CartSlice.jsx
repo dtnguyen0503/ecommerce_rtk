e43081f2f418c2ef9ca55d2e5aef6f5e0da4a2ab
Dat Nguyen <nguyendat9453@gmail.com>
2025-05-09 17:26:55 -0400

    applying logic for Redux toolkit

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
+const initialState = {
+    cartItems: [],
+};
+
+const CartSlice = createSlice ({
+    name: 'cart',
+    initialState,
+    reducers: {
+
+    }
 });
 
 
